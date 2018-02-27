79c5fb576d4bf209779e67b50936113e9b6697b7
xuqingjian8@gmail.com <dora1234>
2018-02-27 11:28:31 +0800

    test drone

--- deploy/collect_static.sh
+++ deploy/collect_static.sh
@@ -1,6 +1,6 @@
 echo $(pwd)
-cd packages/aliyun-python-sdk-core/
+cd deploy/packages/aliyun-python-sdk-core/
 ls
 pip install .
-cd packages/aliyun-python-sdk-dysmsapi/
+cd deploy/packages/aliyun-python-sdk-dysmsapi/
 pip install .
