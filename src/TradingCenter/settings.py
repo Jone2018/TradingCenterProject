5d64940aab3263f95f6d2a3dd0150edeb7abdcaa
xuqingjian8@gmail.com <dora1234>
2018-02-27 13:35:31 +0800

    test drone

--- src/TradingCenter/settings.py
+++ src/TradingCenter/settings.py
@@ -173,7 +173,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = 'https://s3-ap-northeast-1.amazonaws.com/tcstaticfiles/static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # non-default
